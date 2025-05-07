c9ae2f52d56604522b5972373ea81761cfd8da79
Robelsisay <Robelsis677@outlook.com>
2025-05-07 20:33:04 +0300

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,8 +3,6 @@ import './globals.css'
 
 export const metadata: Metadata = {
   title: 'Robel Fit',
-  description: 'Created with v0',
-  generator: 'v0.dev',
 }
 
 export default function RootLayout({
